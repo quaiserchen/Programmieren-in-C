1b771b0a5d02a0d1a440ee1f5fe51e9b2a2fbebe
quaiserchen <113685733+quaiserchen@users.noreply.github.com>
2022-12-06 07:06:24 +0000

    added Geldautomat

---
+++ Klausurvorbereitung/Geldautomat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+
+    int eingabe = 0, rest = 1, tempscheine = 0;
+    int scheine[6] = {500, 200, 100, 50, 20, 10};
+
+    printf("\nBitte geben Sie den Betrag den Sie abheben wollen ein (maximal 4000 Euro) :");
+    scanf("%d", &eingabe);
+
+    if (eingabe > 4000 || eingabe <= 0 || eingabe % 10 != 0)
+    {
+        printf("\nUngueltige Eingabe\n");
+    } else {
+
+        for( int i = 0; i < 6; i++ ) {
+            tempscheine = eingabe / scheine[i];
+            eingabe = eingabe % scheine[i];
+            printf("\nAnzahl der %d Euro-Scheine : %d", scheine[i], tempscheine);
+        }
+    }
+
+        return 0;
+
+    }
